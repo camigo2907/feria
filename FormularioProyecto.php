c4e20b6522b3d93bdedbeb775f00b5d673b35299
camigo2907 <CatalinaBeatriz@10.50.110.84>
2016-05-11 16:33:15 -0300

    Paginas editadas

--- FormularioProyecto.php
+++ FormularioProyecto.php
@@ -8,41 +8,44 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 /**
  *
-* @package local
-* @subpackage feria
-* @copyright 2016 Catalina Amigo Martinez <camigomartinez@gmail.com>
-* @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
-*/
+ * @package local
+ * @subpackage feria
+ * @copyright 2016 Catalina Amigo Martinez <camigomartinez@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *         
+ */
 // Minimum for Moodle to work, the basic libraries
-require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+require_once (dirname ( dirname ( dirname ( __FILE__ ) ) ) . '/config.php');
 
 // Parameter passed from the url.
 $name = 'hola';
 
 // Moodle pages require a context, that can be system, course or module (activity or resource)
-$context = context_system::instance();
-$PAGE->set_context($context);
+$context = context_system::instance ();
+$PAGE->set_context ( $context );
 
 // Check that user is logued in the course.
-require_login();
+require_login ();
 
 // Page navigation and URL settings.
-$PAGE->set_url(new moodle_url('/local/feria', array('filter'=>$name)));
-$PAGE->set_pagelayout('incourse');
-$PAGE->set_title('Feria de Proyectos');
+$PAGE->set_url ( new moodle_url ( '/local/feria', array (
+		'filter' => $name 
+) ) );
+$PAGE->set_pagelayout ( 'incourse' );
+$PAGE->set_title ( 'Feria de Proyectos' );
 // Show the page header
 
 // Here goes the content
-echo $OUTPUT->header();
+echo $OUTPUT->header ();
 
-echo'<form action="guardarproyecto.php" method="post" enctype="multipart/form-data">
+echo '<form action="guardarproyecto.php" method="post" enctype="multipart/form-data">
 <table>
 <tr><td>Nombre del proyecto: </td><td><input type="text" name="nombre" />*</td></tr>
 <tr><td>Descripcion:</td><td><input type="text" name="descripcion" />*</td></tr>
@@ -58,13 +61,13 @@ echo'<form action="guardarproyecto.php" method="post" enctype="multipart/form-da
 <option value="negocios">Negocios</option>
 <option value="quimica">Qu&iacutemica</option>
 </select>*</td></tr>
- <tr><td>Foto:</td><td><input TYPE="file" name="uploadedfile" id="uploadedfile" />*</td></tr>
+ <tr><td>Foto:</td><td><input TYPE="file" name="foto1" id="foto1" />*</td></tr>
 <tr><td></td><td><input TYPE="file" name="foto2" id=”foto2” /></td></tr>
 <tr><td></td><td><input TYPE="file" name="foto3" id=”foto3” /></td></tr>
 <tr><td></td><td><input TYPE="file" name="foto4" id=”foto4”/></td></tr>
-<tr><td>Archivo: </td><td> <input type="file" name="urlarchivo" id=”urlarchivo” />*</td></tr>
+<tr><td>Archivo: </td><td> <input type="file" name="archivo" id=”archivo” />*</td></tr>
  <tr><td>Url de video: </td><td><input type="text" name="urlvideo" /></td></tr>
  <tr><td></td><td><p><input name=enviardatos type="submit" /></p></td><td></td></tr>
 <tr><td></td><td> * Campos Obligatorios</td></tr></table></form> ';
 // Show the page footer
-echo $OUTPUT->footer();
+echo $OUTPUT->footer ();
