0b890ef9b08436e247bf3e5bf9b9a12eecf7a509
camigo2907 <CatalinaBeatriz@10.50.110.124>
2016-05-25 17:23:11 -0300

    ya

--- db/upgrade.php
+++ db/upgrade.php
@@ -4,24 +4,36 @@ function xmldb_local_feria_upgrade($oldversion) {
 	$dbman = $DB->get_manager();
 
 	/// Add a new column newcol to the mdl_myqtype_options
-	if ($oldversion < 2016051705) {
-	  if ($oldversion < 2016051705) {
+ if ($oldversion < 2016052501) {
 
-        // Define field id to be added to proyecto.
+        // Define table proyecto to be created.
         $table = new xmldb_table('proyecto');
-        $field = new xmldb_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
 
-        // Conditionally launch add field id.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
+        // Adding fields to table proyecto.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('nombre', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('categoria', XMLDB_TYPE_CHAR, '300', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('urlarchivo', XMLDB_TYPE_CHAR, '300', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('urlfoto1', XMLDB_TYPE_CHAR, '300', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('urlfoto2', XMLDB_TYPE_CHAR, '300', null, null, null, null);
+        $table->add_field('urlfoto3', XMLDB_TYPE_CHAR, '300', null, null, null, null);
+        $table->add_field('urlfoto4', XMLDB_TYPE_CHAR, '300', null, null, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('urlvideo', XMLDB_TYPE_CHAR, '300', null, null, null, null);
+        $table->add_field('descripcion', XMLDB_TYPE_CHAR, '500', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table proyecto.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, array('userid'), 'user', array('id'));
+
+        // Conditionally launch create table for proyecto.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
         }
 
         // Feria savepoint reached.
-        upgrade_plugin_savepoint(true, 2016051705, 'local', 'feria');
+        upgrade_plugin_savepoint(true, 2016052501, 'local', 'feria');
     }
-	  
-		
-	}
 
 	return true;
 }
